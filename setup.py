85d847d7c4bd61e667639a478de0b4808739c24e
matsunoshin <pmm.avigs.biz@gmail.com>
2020-05-12 15:41:06 +0900

    test for make trypip be importable

--- setup.py
+++ setup.py
@@ -21,8 +21,7 @@ setuptools.setup(
     # long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Matsunoshin/trypip",
-    # packages=setuptools.find_packages(),
-    packages=[PACKAGE_NAME],
+    packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
     classifiers=[
